ccac47901abf298cdbfae1c58b9e0cf0edcef05a
Ramakant <gangwar.ramakant@gmail.com>
2022-05-03 19:24:06 +0530

    💚_Seeder-count

--- database/seeders/LineItemsTableSeeder.php
+++ database/seeders/LineItemsTableSeeder.php
@@ -18,7 +18,7 @@ class LineItemsTableSeeder extends Seeder
             return false;
         }
 
-        $count = 100;
+        $count = 20;
         LineItem::factory()
             ->count($count)
             ->state(function (array $attributes) use ($count) {
